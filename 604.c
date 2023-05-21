7cdebf9f454639a4206f76de655ac21d62c38f7d
Vitaliy Perekhovy <feriman@gmail.com>
2023-05-21 14:47:29 +0300

    Add 604.c

---
+++ 604.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+
+int
+main(int argc, char *argv[])
+{
+	const int NBITS = 32;
+	in_addr_t addr;
+	char      addr_str[INET_ADDRSTRLEN];
+	int       i;
+
+	if (argc != 2) {
+		fprintf(stderr, "Usage: %s <xxx.xxx.xxx.xxx>\n", argv[0]);
+		return EXIT_FAILURE;
+	}
+
+	if (inet_pton(AF_INET, argv[1], &addr) != 1) {
+		fprintf(stderr, "IPv4 address incorrect.\n");
+		return EXIT_FAILURE;
+	}
+
+	for (i = 0; i < NBITS; i++) {
+		addr = ntohl(addr);
+		addr >>= i;
+		addr <<= i;
+		addr = ntohl(addr);
+		inet_ntop(AF_INET, &addr, addr_str, INET_ADDRSTRLEN);
+		printf("%s/%u\n", addr_str, NBITS - i);
+	}
+	addr = 0;
+	inet_ntop(AF_INET, &addr, addr_str, INET_ADDRSTRLEN);
+	printf("%s/%u\n", addr_str, NBITS - i);
+
+	return EXIT_SUCCESS;
+}
+
